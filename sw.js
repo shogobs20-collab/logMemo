05968b254e315478a866383503fe4ab39e35899b
shogobs20-collab <shogo.bs20@gmail.com>
2026-09-10 16:20:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+/* メモアプリ サービスワーカー
+   ★デプロイのたびに CACHE の数字を必ず1つ上げること★ */
+const CACHE = 'memo-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  // GASへの通信は絶対にキャッシュしない
+  if (url.hostname.endsWith('googleusercontent.com') || url.hostname === 'script.google.com') return;
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      if (res.ok && url.origin === location.origin) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
